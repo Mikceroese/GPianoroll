bd286433f198f57c43d97ecd9b140bbdfe25eb8c
Miguel Marcos <m.marcos@unizar.es>
2024-09-10 11:54:47 +0200

    Naming and styling

--- bayesopt_async.py
+++ bayesopt_async.py
@@ -21,48 +21,84 @@ import numpy as np
 
 class BayesOptAsynchronous(BayesOptContinuous):
 
-    # We require an auxiliar thread for optimizing without blocking,
+    # We provide an auxiliar thread for optimizing without blocking,
     # a response event to notify when a result has been given,
     # and a variable for said result.
+    # A 'query' variable provides access from the outside and the
+    # 'query_ready' event can be used for waiting for it.
 
     def __init__(self,n):
 
         super().__init__(n)
 
         self.opt_thread = Thread(group=None,target=super().optimize)
-        self.response = Event()
-        self.result = None
 
+        self.result = None
+        self.result_ready = Event()
+
+        self.query = None
+        self.query_ready = Event()
+
+    # -.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-
+    # Result functions
+    # -.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-
+
+    # Call to set the result *before* notiying
     def set_result(self,result):
         self.result = result
 
-    def notify_response(self):
-        self.response.set()
+    # Call to wake sleeping threads *after* setting the result
+    def notify_result(self):
+        self.result_ready.set()
 
-    def wait_for_response(self):
-        self.response.wait()
+    # (Internal function, do not call from outside)
+    # Sends the optimization thread to sleep
+    def wait_for_result(self):
+        self.result_ready.wait()
 
-    def wait_for_finish(self):
-        self.opt_thread.join()
+    # -.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-
+    # Query functions
+    # -.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-
 
-    # We make a new function for all processing related to the query
+    # New function for all processing related to the query
     # (e.g. graphic representation, forward pass...)
-    # This is the function subclasses should override instead of 'evaluateSample'
-
+    # This is the function subclasses should override 
+    # instead of 'evaluateSample'
     def make_query(self,query):
+        self.query = query
         print(query)
 
-    # Lastly, we redefine the BayesOpt library functions 
-    # to account for the asynchronous execution.
+    # Call to ensure query is ready and updated
+    def wait_for_query(self):
+        self.query_ready.wait()
+        self.query_ready.clear()
 
+    # Call to retrieve the query
+    def get_query(self):
+        return self.query
+
+    # -.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-
+    # Optimization functions
+    # -.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-
+
+    # Redefinition of the BayesOpt library functions 
+    # to account for the asynchronous execution.
     def evaluateSample(self,query):
         self.make_query(query)
-        self.wait_for_response()
-        self.response.clear()
+        self.query_ready.set()
+        self.wait_for_result()
+        # The optimization thread is blocked here until 
+        # 'notify_result' is called.
+        self.result_ready.clear()
         return self.result
     
+    # Starts the optimization thread
     def optimize(self):
         self.opt_thread.start()
+
+    # Joins the optimization thread. Call on termination.
+    def wait_for_finish(self):
+        self.opt_thread.join()
 
 if __name__ == "__main__":
 
@@ -82,7 +118,7 @@ if __name__ == "__main__":
     bo.optimize()
 
     for i in range(params['n_init_samples']+params['n_iterations']):
-        x = float(input()) # Change the result retrieval method as needed
+        x = float(input()) # Change the retrieval method as needed
         bo.set_result(x)
         bo.notify_response()
 
