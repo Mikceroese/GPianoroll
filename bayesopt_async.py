48959d3f52e89500981e5a17e8954fa172283dce
Miguel Marcos <m.marcos@unizar.es>
2024-09-11 14:21:52 +0200

    WeBO v1 ready

--- bayesopt_async.py
+++ bayesopt_async.py
@@ -11,44 +11,52 @@
 # use its functionalities just by overriding    #
 # the 'make_query' method.                      #
 #                                               #
+# A toy example is provided as the main program #
+#                                               #
 # Author: Miguel Marcos (m.marcos@unizar.es)    #
 #################################################
 
 from bayesoptmodule import BayesOptContinuous
-from threading import Thread, Event
+from threading import Thread, Event, BoundedSemaphore
 
 import numpy as np
 
 class BayesOptAsynchronous(BayesOptContinuous):
 
-    # We provide an auxiliar thread for optimizing without blocking,
-    # a response event to notify when a result has been given,
-    # and a variable for said result.
-    # A 'query' variable provides access from the outside and the
-    # 'query_ready' event can be used for waiting for it.
-
     def __init__(self,n):
 
         super().__init__(n)
 
-        self.opt_thread = Thread(group=None,target=super().optimize)
-
-        self.result = None
-        self.result_ready = Event()
-
+        # Storing and notifying a new query
         self.query = None
         self.query_ready = Event()
 
+        # Storing and notifying query evaluations
+        self.result = None
+        self.result_ready = Event()
+        self.eval_done = Event()
+
+        # Handling progress and termination
+        self.it_semaphore = BoundedSemaphore()
+        self.remaining_iterations = -1
+        self.opt_done = Event()
+
+        # Variables for storing the final optimization result
+        self.final_result = {}
+        self.mvalue = None
+        self.x_out = None
+        self.error = False
+
+        # Background thread, for non-blocking execution
+        self.opt_thread = Thread(group=None,target=self.targetFunction)
+
     # -.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-
-    # Result functions
+    # Query result functions
     # -.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-
 
-    # Call to set the result *before* notiying
+    # Call to set the result and notiying it
     def set_result(self,result):
         self.result = result
-
-    # Call to wake sleeping threads *after* setting the result
-    def notify_result(self):
         self.result_ready.set()
 
     # (Internal function, do not call from outside)
@@ -88,17 +96,60 @@ class BayesOptAsynchronous(BayesOptContinuous):
         self.query_ready.set()
         self.wait_for_result()
         # The optimization thread is blocked here until 
-        # 'notify_result' is called.
+        # 'set_result' is called.
         self.result_ready.clear()
+        self.it_semaphore.acquire()
+        self.remaining_iterations -= 1
+        if self.remaining_iterations == 0:
+            self.opt_done.set()
+        self.it_semaphore.release()
+        self.eval_done.set()
         return self.result
     
-    # Starts the optimization thread
+    # Call this to ensure the number of remaining iterations and 
+    # the termination flag have been updated after a query.
+    # Returns whether this was the last evaluation or not.
+    def wait_for_eval(self):
+        self.eval_done.wait()
+        self.eval_done.clear()
+        return self.opt_done.is_set()
+    
+    # Proxy function for the optimization thread
+    # (Threads can't return values, but values stored inside are saved)
+    def targetFunction(self):
+        self.mvalue, self.x_out, self.error = super().optimize()
+    
+    # Starts the optimization thread (only once)
     def optimize(self):
-        self.opt_thread.start()
+        start = not self.has_started()
+        if start:
+            self.it_semaphore.acquire()
+            self.remaining_iterations = self.params['n_init_samples'] + \
+                                        self.params['n_iterations']
+            self.it_semaphore.release()
+            self.opt_thread.start()
+        return start
 
     # Joins the optimization thread. Call on termination.
     def wait_for_finish(self):
         self.opt_thread.join()
+
+    # If the number of iterations is not -1, 'optimize' was called.
+    def has_started(self):
+        self.it_semaphore.acquire()
+        started = self.remaining_iterations >= 0
+        self.it_semaphore.release()
+        return started
+
+    # Check for termination flag
+    def has_finished(self):
+        return self.opt_done.is_set()
+    
+    # Return a dictionary with the result variables
+    def get_results(self):
+        return {'mvalue':self.mvalue, 
+                'x_out':self.x_out, 
+                'error':self.error}
 
 if __name__ == "__main__":
 
@@ -117,12 +168,15 @@ if __name__ == "__main__":
 
     bo.optimize()
 
-    for i in range(params['n_init_samples']+params['n_iterations']):
-        x = float(input()) # Change the retrieval method as needed
+    while True:
+        x = float(input()) # Change the input method as needed
         bo.set_result(x)
-        bo.notify_response()
+        finish = bo.wait_for_eval()
+        if finish:
+            break
 
     bo.wait_for_finish()
 
     print("Done")
+    print(bo.get_results())
     
---
+++ bayesopt_web.py
@@ -0,0 +1,253 @@
+##################################################
+# This file contains the BayesOptWeb             #
+# subclass for the BayesOpt module, as well as a #
+# pool manager for concurrency in web apps.      #
+#                                                #
+# The objective of this class is to provide a    #
+# web interface that uses the BayesOptAsync      #
+# subclass capabilities and the Flask library    #
+# to render a view of an objective function      #
+# evaluation and an input for the given result   #
+# in a web browser.                              #
+#                                                #
+# A toy example is provided as the main program  #
+#                                                #
+# Author: Miguel Marcos (m.marcos@unizar.es)     #
+##################################################
+
+from bayesopt_async import BayesOptAsynchronous
+from flask import Flask, redirect, url_for, request, session, render_template
+from threading import BoundedSemaphore
+from datetime import datetime
+
+import numpy as np
+
+class BayesOptWeb(BayesOptAsynchronous):
+
+    # We require a template (as a name string) to render the view.
+    # A Flask web application should call the make_query function,
+    # which will make the template filled with the appropriate
+    # representation of the query accesible with 'get_query'.
+
+    def __init__(self,n,template):
+
+        super().__init__(n)
+
+        # Flask looks for templates in a 'templates' folder
+        # Enter the name as a relative path to that folder.
+        self.query_template = template
+
+    # For demonstration purposes, we replace the 'print' call of
+    # BayesOptAsynchronous with plain text in an HTML page.
+    # For more info on Flask templates, check 
+    # https://www.tutorialspoint.com/flask/flask_templates.htm
+
+    def make_query(self,query):
+        self.query = render_template(self.query_template, 
+                                     query=str(query))
+        
+    # Flask messes with the application context
+    # The 'with' statement prevents crash scenarios.
+    def targetFunction(self):
+        global bo_app
+        with bo_app.app_context():
+            super().targetFunction()
+
+    # Numpy arrays are not json resializable
+    def get_results(self):
+        return {'mvalue':self.mvalue, 
+                'x_out':self.x_out.tolist(), 
+                'error':self.error}
+
+class BayesOptWebPool():
+
+    # The BayesOptWebPool class is a dictionary of
+    # simultaneous BayesOpt experiments with potentially different
+    # problem dimensions and HTML templates. So far it only supports
+    # BayesOptWeb instances, so any special features should be
+    # implemented on the front end.
+
+    # DISCLAIMER: This class assumes polite behavior from users 
+    # (i.e. no unfinished experiments) and has only basic security and 
+    # resilience measures against errors. Do not use on production or
+    # public networks.
+
+    def __init__(self, max_exp = 10):
+
+        self.max_exp = max_exp
+        self.pool = {}
+        self.next_id = 0
+        self.id_semaphore = BoundedSemaphore()
+
+    # Adds a new experiment to the pool. Returns the ID of the new
+    # experiment (which may not be the provided one, if any was) and
+    # an error flag (which is set to False on success)
+    def new_exp(self,n,template,id=None):
+
+        self.id_semaphore.acquire()
+        if len(self.pool) < self.max_exp:
+            if id is None:
+                id = str(self.next_id) + \
+                     datetime.now().strftime("%m-%d-%Y_%H:%M:%S")
+            else:
+                id = str(self.next_id) + str(id) + \
+                     datetime.now().strftime("%m-%d-%Y_%H:%M:%S")
+            self.next_id += 1
+            self.pool[id] = BayesOptWeb(n,template)
+            self.id_semaphore.release()
+            return id, False
+        else:
+            self.id_semaphore.release()
+            return -1, True
+
+    # Sets the parameters of the experiment with the given ID, if there
+    # is one. Returns whether this was True or not.
+    def set_exp_params(self,id,params,lb,ub):
+        ret = id in self.pool
+        if ret:
+            self.pool[id].params = params
+            self.pool[id].lower_bound = lb
+            self.pool[id].upper_bound = ub
+        return ret
+    
+    # Launches the optimization thread of the experiment with the 
+    # given ID, if there is one and it hasn't been launched yet. 
+    # Returns whether this was True or not.
+    def start_exp(self,id):
+        ret = id in self.pool
+        if ret:
+            ret = self.pool[id].optimize()
+        return ret
+    
+    # Waits for an indicated experiment's query to be ready, or returns
+    # immediately if there is no experiment with the given ID.
+    def wait_for_exp_query(self,id):
+        if id in self.pool:
+            self.pool[id].wait_for_query()
+            return self.pool[id].get_query()
+        else:
+            return None       
+    
+    # Sets the result for an experiment's query, if an experiment with
+    # the provided ID is in the pool.
+    # Returns whether this was True or not.
+    def set_exp_query_result(self,id,result):
+        ret = id in self.pool
+        if ret:
+            self.pool[id].set_result(result)
+        return ret
+
+    # Waits for an indicated experiment's query result to be registered,
+    # or returns immediately if the ID is not in the pool.
+    # If there was such an experiment, the function returns whether the
+    # experiment is finished or not.
+    # Call after setting a query result.
+    def wait_for_exp_eval(self,id):
+        if id in self.pool:
+            return self.pool[id].wait_for_eval()
+        else:
+            return None           
+    
+    # Removes the experiment with the given ID from the pool if it
+    # exists and it has finished or hasn't started. Returns whether this
+    # was True or not, along with the results of the experiment. 
+    def remove_exp(self,id):
+        ret = (id in self.pool) and (self.pool[id].has_finished() or
+                                     not self.pool[id].has_started())
+        results = None
+        if ret:
+            results = self.pool[id].get_results()
+            self.pool.pop(id)
+        return results, ret
+
+# -.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-
+
+bo_app = Flask(__name__)
+bo_app.secret_key = 'everything is Gaussian'
+bo_web_pool = BayesOptWebPool()
+
+@bo_app.route('/')
+def index():
+   return render_template('basic_bo_index.html')
+
+@bo_app.route('/start', methods = ['GET', 'POST'])
+def start_exp():
+
+    if request.method == 'GET':
+        return redirect(url_for('index'))
+    else:
+
+        global bo_web_pool
+        
+        bo_id, err = bo_web_pool.new_exp(2,'basic_bo_query.html',
+                                         id=request.form['name'])
+        if err:
+            return redirect(url_for('index'))
+        else:
+            session['bo_id'] = bo_id
+            params = {}
+            n = 2
+            lb = np.zeros((n,)) # Lower bounds
+            ub = np.ones((n,)) # Upper bounds
+            params['n_init_samples'] = 2
+            params['n_iterations'] = 2
+            params['load_save_flag'] = 2 # Save
+            params['save_filename'] = "exps/"+session['bo_id']+".txt"
+            params['verbose_level'] = 5 # Debug -> logfile
+            params['log_filename'] = "exps/"+session['bo_id']+"_log.txt"
+            bo_web_pool.set_exp_params(bo_id,params,lb,ub)
+            bo_web_pool.start_exp(bo_id)
+            return redirect(url_for('query'))
+
+@bo_app.route('/query')
+def query():
+    if 'bo_id' not in session:
+        redirect(url_for('index'))
+    else:
+
+        global bo_web_pool
+
+        bo_id = session['bo_id']
+        query_template = bo_web_pool.wait_for_exp_query(bo_id)
+        return query_template
+    
+@bo_app.route('/input_result', methods=['POST'])
+def query_input():
+    if 'bo_id' not in session:
+        redirect(url_for('index'))
+    else:
+
+        global bo_web_pool
+
+        bo_id = session['bo_id']
+        q_result = float(request.form['result'])
+        bo_web_pool.set_exp_query_result(bo_id,q_result)
+        finished = bo_web_pool.wait_for_exp_eval(bo_id)
+        if not finished:
+            return redirect(url_for('query'))
+        else:
+            bo_result, ret = bo_web_pool.remove_exp(bo_id)
+            session['bo_result'] = bo_result
+            return redirect(url_for('end_exp'))
+    
+
+@bo_app.route('/result')
+def end_exp():
+    if ('bo_id' not in session) or ('bo_result' not in session):
+        redirect(url_for('index'))
+    else:
+
+        global bo_web_pool
+
+        bo_result = session['bo_result']
+        return render_template('basic_bo_result.html',res=bo_result)
+    
+@bo_app.route('/logout',methods=['GET','POST'])
+def logout():
+    session.pop('bo_id')
+    session.pop('bo_result')
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+   bo_app.run(debug = True)
+    
